b70a3cb20a7c6110724e77fdc0f930a84e13ebf7
clown <89036755470a@gmail.com>
2026-02-12 20:35:44 +0300

    feat(binary-search): implement binary-search algorithm

---
+++ Exercism/c/binary-search/binary_search.c
@@ -0,0 +1,22 @@
+#include "binary_search.h"
+#include <stddef.h>
+
+const int *binary_search(int value, const int *arr, size_t length)
+{
+    if (arr == NULL || length == 0) return NULL;
+    int low = 0, high = length - 1;
+
+        while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == value) {
+            return &arr[mid];
+        }
+        else if (arr[mid] > value) {
+            high = mid - 1;
+        }
+        else if (arr[mid] < value) {
+            low = mid + 1;
+        }
+    }
+    return NULL;
+}
