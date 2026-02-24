11aedf50dfb49bc3e2b4b76a51c479961fc1ca7e
clown <89036755470a@gmail.com>
2026-02-24 10:46:05 +0300

    feat(collatz-conjecture): implement steps function to handle odd/even numbers

---
+++ Exercism/c/collatz-conjecture/collatz_conjecture.c
@@ -0,0 +1,17 @@
+#include "collatz_conjecture.h"
+
+int steps(int start)
+{
+    if (start < 1) return -1;
+    int count = 0;
+    while (start != 1) {
+        if (start % 2 == 0) {
+            start /= 2;
+        } else {
+            start = 3 * start + 1;
+            
+        }
+        count++;
+    }
+    return count;
+}
