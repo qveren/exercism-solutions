cb9f7d2436bb57c40b4dd2d3c29d5e1d33edbf11
clown <89036755470a@gmail.com>
2026-02-24 12:28:39 +0300

    feat(space-age): implement age functions for planets using orbital periods

---
+++ Exercism/c/space-age/space_age.c
@@ -0,0 +1,22 @@
+#include "space_age.h"
+
+const float ORBITAL[] = {
+   0.2408467,
+   0.61519726,
+   1.0,
+   1.8808158,
+   11.862615,
+   29.447498,
+   84.016846,
+   164.79132
+};
+
+float age(planet_t planet, int64_t seconds) 
+{
+    int64_t age_on_earth = seconds / 31557600.0;
+    if (planet < MERCURY || planet > NEPTUNE) return -1.0;
+
+    float age_on_planet = age_on_earth / ORBITAL[planet];
+    
+    return age_on_planet;
+}
