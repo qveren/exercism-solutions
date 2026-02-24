103385a67618ae548e220be1084053987efe0e39
clown <89036755470a@gmail.com>
2026-02-24 12:32:26 +0300

    fix(space-age): use float division for seconds

--- Exercism/c/space-age/space_age.c
+++ Exercism/c/space-age/space_age.c
@@ -13,7 +13,7 @@ const float ORBITAL[] = {
 
 float age(planet_t planet, int64_t seconds) 
 {
-    int64_t age_on_earth = seconds / 31557600.0;
+    int64_t age_on_earth = seconds / 31557600.0f;
     if (planet < MERCURY || planet > NEPTUNE) return -1.0;
 
     float age_on_planet = age_on_earth / ORBITAL[planet];
