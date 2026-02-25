52803c601329bbebeba26635a0333666a0b37ebc
clown <89036755470a@gmail.com>
2026-02-25 13:57:53 +0300

    feat(perfect-numbers): implement number classify numbers as perfect adundant and deficient

--- Exercism/c/perfect-numbers/perfect_numbers.c
+++ Exercism/c/perfect-numbers/perfect_numbers.c
@@ -2,14 +2,14 @@
 
 kind classify_number(int number)
 {
-    if (number < 1) return -1;
+    if (number < 1) return ERROR;
     int sum = 0;
     for (int i = 1; i <= number / 2; i++) {
         if (number % i == 0) {
             sum += i;
         }
     }
-    if (sum == number) return 1;
-    else if (sum > number) return 2;
-    else return 3;
+    if (sum == number) return PERFECT_NUMBER;
+    else if (sum > number) return ABUNDANT_NUMBER;
+    else return DEFICIENT_NUMBER;
 }
