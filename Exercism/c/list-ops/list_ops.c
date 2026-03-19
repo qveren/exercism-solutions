499cfcdb72a686b0af1f2e71edb0161d6d743cc5
clown <89036755470a@gmail.com>
2026-03-19 22:16:38 +0300

    feat(list_ops): implement basic list operations with flexible array member

---
+++ Exercism/c/list-ops/list_ops.c
@@ -0,0 +1,83 @@
+#include "list_ops.h"
+#include <stdlib.h>
+#include <string.h>
+
+// constructs a new list
+list_t *new_list(size_t length, list_element_t elements[])
+{
+    if (elements == 0 || length == 0) return NULL;
+    list_t *list = malloc(sizeof(list_t) + length * sizeof(list_element_t));
+    if (!list) return NULL;
+
+    list->length = length;
+    memcpy(list->elements, elements, length * sizeof(list_element_t));
+    return list;
+}
+
+// append entries to a list and return the new list
+list_t *append_list(list_t *list1, list_t *list2)
+{
+    if (list1 == NULL || list2 == NULL) return NULL;
+    size_t full_length = list1->length + list2->length;
+    size_t full_size = sizeof(list_t) + full_length * sizeof(list_element_t);
+    list_t *tmp = realloc(list1, full_size);
+    if (!tmp) return NULL;
+    list1 = tmp;
+    memcpy(list1->elements + list1->length, list2->elements, list2->length * sizeof(list_element_t));
+    list1->length = full_length;
+    return list1;
+}
+
+// filter list returning only values that satisfy the filter function
+list_t *filter_list(list_t *list, bool (*filter)(list_element_t))
+{
+    size_t count = 0;
+    for (size_t i = 0; i < list->length; ++i) {
+        if (filter(list->elements[i])) {
+            count++;
+        }
+    }
+    list_t *list_filter = malloc(sizeof(list_t) + count * sizeof(list_element_t));
+    if (!list_filter) return 0;
+
+    size_t idx = 0;
+    for (size_t i = 0; i < list->length; ++i) {    
+        if (filter(list->elements[i])) {
+            list_filter->elements[idx++] = list->elements[i];
+        }
+    }
+    list_filter->length = count;
+    
+    return list_filter;
+}
+
+// returns the length of the list
+size_t length_list(list_t *list) 
+{
+    if (!list) return 0;
+    return list->length;
+}
+
+// return a list of elements whose values equal the list value transformed by
+// the mapping function
+list_t *map_list(list_t *list, list_element_t (*map)(list_element_t));
+
+// folds (reduces) the given list from the left with a function
+list_element_t foldl_list(list_t *list, list_element_t initial,
+                          list_element_t (*foldl)(list_element_t,
+                                                  list_element_t));
+
+// folds (reduces) the given list from the right with a function
+list_element_t foldr_list(list_t *list, list_element_t initial,
+                          list_element_t (*foldr)(list_element_t,
+                                                  list_element_t));
+
+// reverse the elements of the list
+list_t *reverse_list(list_t *list);
+
+// destroy the entire list
+// list will be a dangling pointer after calling this method on it
+void delete_list(list_t *list) 
+{
+    free(list);
+}
